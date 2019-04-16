59946e9cf80b5feef018929fd25d7052be6dfdea
sudheermareddy <sudheerm@sysgain.com>
2019-04-16 18:37:19 +0530

    Add files via upload

---
+++ happyworld/index.js
@@ -0,0 +1,16 @@
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("happy world !");
+
+});
+
+var port = 5000;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
+
+
+// my comments on this file
