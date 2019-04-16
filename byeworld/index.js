cdcc471ed52f0128d45773f97196e0db6b09173b
sudheermareddy <sudheerm@sysgain.com>
2019-04-16 18:14:18 +0530

    Add files via upload

---
+++ byeworld/index.js
@@ -0,0 +1,16 @@
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Bye world !");
+
+});
+
+var port = 80;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
+
+
+// my comments on this file
