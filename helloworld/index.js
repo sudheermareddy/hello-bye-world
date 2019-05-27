a4f0605e6c880a110cdeded9ee1850846bef7241
sudheermareddy <sudheerm@sysgain.com>
2019-05-27 12:40:54 +0530

    up

--- helloworld/index.js
+++ helloworld/index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hellow happy world.......!");
+    response.end("Hello happy world.......!");
 
 });
 
